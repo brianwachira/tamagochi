dd1965231d47bbcbd19c2b30112de9369bca738d
brianwachira <brianwachira7@gmail.com>
2020-07-11 21:51:59 +0300

    add jquery popper and bootstrap imports:

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+
+import {Tamagochi} from './js/tamagochi.js';
+
+$(document).ready(function(){
+
+  
+});
